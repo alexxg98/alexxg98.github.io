79fd6604162ca3c3ffda495279f922621b0ed0d7
Alexandria <lexieguo@gmail.com>
2021-01-05 19:10:16 -0500

    Add scroll to top button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from './components/Navigation.js';
 import Home from './components/Home.js';
 import AboutMe from './components/AboutMe.js';
 import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+import upArrow from './images/upArrow.png';
 
 function App() {
+  const [show, setShow] = useState(false);
+  const onScroll = () => {
+    setShow(document.documentElement.scrollTop > 500);
+  }
+  useEffect(() => {
+    document.addEventListener("scroll", onScroll);
+    return () => document.removeEventListener("scroll", onScroll);
+  }, []);
+
   return (
     <div className="App">
 
@@ -16,6 +26,12 @@ function App() {
       <Experience/>
       <Projects/>
       <Contact/>
+
+      {show && (
+        <button id="topButton" onClick={() => window.scrollTo({top: 0, behavior: 'smooth'})}>
+          <img src={upArrow} alt="Back to top"/>
+        </button>
+      )}
 
     </div>
   );
