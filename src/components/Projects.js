0852924119a34022638f900f71f27b1310c2a51a
Alexandria <alexandria.y.guo@gmail.com>
2022-01-14 23:53:04 -0500

    Little changes to UI

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -33,21 +33,21 @@ export default class Projects extends Component {
           <div className="grid-item projImg">
             <img src={RobotsVsAliens} alt="Robots Vs Aliens Demo"/>
           </div>
-          <div className="grid-item" id="rVSaInfo">
+          <div className="grid-item projInfo" id="rVSaInfo">
             <p className="projType">Coursework</p>
             <h6 className="projTitle">Robots vs Aliens</h6>
             <p>Senior Design project at CCNY. A 2D game built on Unity, based on Plants vs Zombies. The objective is defend the homebase for a certain duration and/or collect enough material to rebuild the shield. </p>
             <span className="badge badge-dark badge-pill projSoftware"> Unity </span>
             <span className="badge badge-dark badge-pill projSoftware"> C++ </span>
             <span className="badge badge-dark badge-pill projSoftware"> Pixel Art </span>
-            <br/><br/><button id="sourceCode"><span><a href="https://alexxg98.github.io/Robots-vs-Aliens/" target="_blank" rel="noopener noreferrer"> DEMO/TUTORIAL </a></span></button>
+            <br/><br/><button id="demoSite"><span><a href="https://alexxg98.github.io/Robots-vs-Aliens/" target="_blank" rel="noopener noreferrer"> DEMO/TUTORIAL </a></span></button>
             <br/><button id="sourceCode"><span><a href="https://github.com/alexxg98/SD_FinalProject" target="_blank" rel="noopener noreferrer"> SOURCE CODE </a></span></button>            
           </div>
           
           <div className="grid-item projImg">
             <img src={CreditsTracker} alt="Credits Tracker Demo"/>
           </div>
-          <div className="grid-item" id="credTrackInfo">
+          <div className="grid-item projInfo" id="credTrackInfo">
             <p className="projType">Coursework</p>
             <h6 className="projTitle">Credits Tracking App</h6>
             <p>Final project for Software Design course at CCNY. An Android app for CCNY Computer Science students. The app records the courses the student has taken, are in the process of taking, and have not taken yet.</p>
@@ -59,7 +59,7 @@ export default class Projects extends Component {
           <div className="grid-item projImg">
             <img src={TheHive} alt="The Hive Demo"/>
           </div>
-          <div className="grid-item" id="theHiveInfo">
+          <div className="grid-item projInfo" id="theHiveInfo">
             <p className="projType">Coursework</p>
             <h6 className="projTitle">Project Management System</h6>
             <p>Final project for Software Engineering course at CCNY. This system will facilitate active teaming of people with similar interest and skill-set to forge groups for a certain do-good project.</p>
